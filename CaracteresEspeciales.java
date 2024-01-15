d9826b23c57d9804c69f9a529d350a0f1454b664
202213868e@utea.edu.pe <202213868e@utea.edu.pe>
2024-01-15 10:31:02 -0500

    primer commit

--- CaracteresEspeciales.java
+++ CaracteresEspeciales.java
@@ -1,11 +1,14 @@
 /**
- * muestra caracteres especiales
- * 
- * @author Danny . Ramirez
- */
+* Salida formateada
+*
+* @author Jose F. Marcas
+*/
 public class CaracteresEspeciales {
     public static void main(String[] args) {
-        System.out.println("¡Hola, mundo! \uD83D\uDE00");
-    
+        // Ejemplo de cadena con caracteres especiales
+        String cadenaConEspeciales = "¡Hola, mundo! Este es un carácter especial: \u00A9";
+
+        // Imprimir la cadena
+        System.out.println(cadenaConEspeciales);
     }
 }
