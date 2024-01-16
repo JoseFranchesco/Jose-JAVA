8e03b118c717d47d2dd54d1aa2bf2c628b34b887
202213868e@utea.edu.pe <202213868e@utea.edu.pe>
2024-01-16 12:37:02 -0500

    Ejercicios

---
+++ Ejercicios/ejercicio8.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+        System.out.println(" /\\_/\\");
+        System.out.println("( o.o )");
+        System.out.println(" > ^ <");
+    }
+}
+
