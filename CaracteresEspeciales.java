4858160e62297d9f9734ea1fc3082b77b41b5556
JoseFranchesco <156683652+JoseFranchesco@users.noreply.github.com>
2024-01-16 21:53:56 -0500

    CaracteresEspeciales.java

--- CaracteresEspeciales.java
+++ CaracteresEspeciales.java
@@ -5,10 +5,7 @@
 */
 public class CaracteresEspeciales {
     public static void main(String[] args) {
-        // Ejemplo de cadena con caracteres especiales
         String cadenaConEspeciales = "¡Hola, mundo! Este es un carácter especial: \u00A9";
-
-        // Imprimir la cadena
         System.out.println(cadenaConEspeciales);
     }
 }
