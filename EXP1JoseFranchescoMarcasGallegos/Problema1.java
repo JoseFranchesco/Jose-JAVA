730d3306a490e9a9cd202e7281bdc90566b17775
202213868e@utea.edu.pe <202213868e@utea.edu.pe>
2024-02-20 12:54:27 -0500

    exp1

--- EXP1JoseFranchescoMarcasGallegos/Problema1.java
+++ EXP1JoseFranchescoMarcasGallegos/Problema1.java
@@ -1,16 +1,33 @@
 package EXP1JoseFranchescoMarcasGallegos;
 
+import java.util.Scanner;
 
 public class Problema1 {
     public static void main(String[] args) {
-        final int TotalNumeros = 1000;
-        final int Numerosengrupo = 20;
+        Scanner s = new Scanner(System.in);//crearemos un nuevo scanner para que lea los datos
 
-        for (int i = 1; i <= TotalNumeros; i++) {
-            System.out.print(i + " ");
-            if (i % Numerosengrupo == 0) {
-                System.out.println();
-            }   
+        System.out.print("Ingrese un numero Inicial: ");//ingresaremos un numero de inicio
+        int inicio = s.nextInt();//lee el numero inicial dado
+        System.out.print("Ingrese un numero Final: ");//ingresaremos un numero de fin
+        int fin = s.nextInt();//lee el numero final dado
+
+        if (fin <= inicio) { //Validaremos que el valor final sea mayor que el inicio
+            System.out.println("Error: El valor de final debe ser mayor que el valor de inicio.");//Mostraremos un mensaje
+            return; //En caso que no cumpla va devolver
+        }
+        System.out.print("Ingrese el tamaño del grupo: ");
+        int tamanoGrupo = s.nextInt();//lee el tamaño del grupo dado
+
+        for (int i = inicio; i <= fin; i++) { //creamos bucle con las condiciones
+            System.out.print(i + " ");//i tiene valor 1 y lo sumaremos con un espacio para 
+
+            if (i % tamanoGrupo == 0) { //verifica si i es divisible por el tamaño del grupo para que siga las condiciones
+                System.out.print("¿Quieres continuar? (Sí/No): "); //Preguntaremos si se desea continuar
+                String continuar = s.next().toLowerCase(); //se encarga de leer los carecteres y conivierte en minuscula
+                if (!continuar.equals("si")) { //continuara mostrando dependiendo de la respuesta obtenida en el bucle for
+                    break; //sirve para romper el bucle
+                }
+            }
         }
     }
 }
--- EXP1JoseFranchescoMarcasGallegos/Problema3.java
+++ EXP1JoseFranchescoMarcasGallegos/Problema3.java
@@ -1,5 +1,17 @@
 package EXP1JoseFranchescoMarcasGallegos;
 
+import java.util.Scanner;
+
 public class Problema3 {
+    public static void main(String[] args) { //iniciara la ejecucion del programa
+        Scanner s = new Scanner(System.in); //crearemos un scanner para la entrada de datos
+        System.out.print("Ingrese la altura que se usara para el objeto: ");
     
+        double altura = s.nextDouble(); //almacenamos los datos en scanner usando la variable altura que nosotros proporcionaremos
+        double gravedad = 9.81; //Establecemos el valor de la gravedad
+        double tiempoDeCaida = Math.sqrt((2 * altura) / gravedad); //Usamos la formuola de tiempo de caida que es Tiempo de caida= raiz de 2 por la altura entre la gravedad
+
+        System.out.println("El objeto tardara en caer: " + tiempoDeCaida + " segundos.");//texta para dar el resultado
+
+    }
 }
